b3c964897273e8a045321a2dd6a53f18050823f5
Lee Geunil <lee_geunil@futurescope.jp>
2016-03-17 10:13:46 +0900

    Initial Commit

---
+++ EmoticonFoutune/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmoticonFoutune
+//
+//  Created by 李根一 on 2016/03/17.
+//  Copyright © 2016年 Lee Geunil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
